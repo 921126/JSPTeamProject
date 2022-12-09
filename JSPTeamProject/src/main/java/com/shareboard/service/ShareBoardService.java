787b67e9c0e1b0e1a994b579d6ad19123d439c36
dldldl <sk223r5@gmail.com>
2022-12-09 11:48:37 +0900

    20221209 11:46 jaeyun 공용게시판작업중

---
+++ JSPTeamProject/src/main/java/com/shareboard/service/ShareBoardService.java
@@ -0,0 +1,13 @@
+package com.shareboard.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface ShareBoardService {
+	
+	//public 생략
+	void regist(HttpServletRequest request, HttpServletResponse response);
+	
+	
+	
+}
